2e6c5da26a3dfa1f4ae904500ab9fe81dfb19fa9
Ashok M B <ashok.mb@nortonlifelock.com>
2020-11-02 22:25:25 +0530

    Remove watermark

--- es_util/es_utils.py
+++ es_util/es_utils.py
@@ -1,25 +1,4 @@
 """
-NortonLifeLock copyright header start
-
- Copyright (c) 2020 NortonLifeLock Inc. All rights reserved.
-
- THIS SOFTWARE CONTAINS CONFIDENTIAL INFORMATION AND TRADE SECRETS OF NORTONLIFELOCK
- INC.  USE, DISCLOSURE OR REPRODUCTION IS PROHIBITED WITHOUT THE PRIOR
- EXPRESS WRITTEN PERMISSION OF NORTONLIFELOCK INC.
-
- The Licensed Software and Documentation are deemed to be commercial computer
- software as defined in FAR 12.212 and subject to restricted rights as defined
- in FAR Section 52.227-19 "Commercial Computer Software - Restricted Rights"
- and DFARS 227.7202, "Rights in Commercial Computer Software or Commercial
- Computer Software Documentation", as applicable, and any successor regulations.
- Any use, modification, reproduction release, performance, display or disclosure
- of the Licensed Software and Documentation by the U.S. Government shall be
- solely in accordance with the terms of this Agreement.
-
- NortonLifeLock copyright header stop
-"""
-
-"""
 Utility module to hold all the helpers required to perform actions in elasticsearch
 """
 
